b4bd7b78ebf4424776240047555f54723bab47e3
xNombre <kartapolska@gmail.com>
2018-12-25 16:12:33 +0100

    Upload package 25.12.18

--- aarch64-xnombre-linux-android/sysroot/usr/include/android/ndk-version.h
+++ aarch64-xnombre-linux-android/sysroot/usr/include/android/ndk-version.h
@@ -6,27 +6,27 @@
  *
  * For example: 16 for r16.
  */
-#define __NDK_MAJOR__ 17
+#define __NDK_MAJOR__ 19
 
 /**
  * Minor version of this NDK.
  *
  * For example: 0 for r16 and 1 for r16b.
  */
-#define __NDK_MINOR__ 1
+#define __NDK_MINOR__ 0
 
 /**
  * Set to 0 if this is a release build, or 1 for beta 1,
  * 2 for beta 2, and so on.
  */
-#define __NDK_BETA__ 0
+#define __NDK_BETA__ 2
 
 /**
  * Build number for this NDK.
  *
  * For a local development build of the NDK, this is -1.
  */
-#define __NDK_BUILD__ 4828580
+#define __NDK_BUILD__ 5160253
 
 /**
  * Set to 1 if this is a canary build, 0 if not.
